e8d32a88e3af7f45828651a959d9f9df4bf6ad27
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-22 23:26:06 +0430

    static expenses array added

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,10 +2,43 @@ import { Text, View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput() {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "a pair of shoes",
+    amount: 44.53,
+    date: new Date("2025-10-01"),
+  },
+  {
+    id: "e2",
+    description: "winter coat",
+    amount: 70.44,
+    date: new Date("2025-10-02"),
+  },
+  {
+    id: "e3",
+    description: "glovesses",
+    amount: 20.53,
+    date: new Date("2025-10-10"),
+  },
+  {
+    id: "e4",
+    description: "fruit",
+    amount: 10.53,
+    date: new Date("2025-10-11"),
+  },
+  {
+    id: "e5",
+    description: "ring",
+    amount: 200.1,
+    date: new Date("2025-10-20"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
