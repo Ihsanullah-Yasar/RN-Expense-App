8fc882d9f446b8066c9a13d0c8b328bc0a6c204b
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-28 23:23:59 +0430

    expense item press style effect added

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -3,8 +3,14 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../utils/date";
 
 function ExpenseItem({ description, amount, date }) {
+  function expensePressHandler() {
+    console.log("expense press handler triggered");
+  }
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -23,6 +29,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    // opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
