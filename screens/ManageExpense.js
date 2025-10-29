b1c263794a54b1fd9ade911a26fcee349799919c
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-29 22:15:03 +0430

    expense screen style improved and delete button added and styled

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import IconButton from "../components/UI/iconButton";
+import { GlobalStyles } from "../constants/styles";
 
 function ManageExpense({ route, navigation }) {
   const editingExpenseId = route.params?.expenseId;
@@ -11,11 +13,37 @@ function ManageExpense({ route, navigation }) {
     });
   }, [isEditing, navigation]);
 
-  return <Text>ManageExpense Screen</Text>;
+  function deleteHandler() {}
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    padding: 8,
+    marginTop: 16,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
 });
