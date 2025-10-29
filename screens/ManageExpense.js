0a06c9c7463d9371df01190295e004d0d1436e2e
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-29 21:55:39 +0430

    dynamic header title based on route parameter

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { StyleSheet, Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editingExpenseId = route.params?.expenseId;
+  const isEditing = !!editingExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [isEditing, navigation]);
+
   return <Text>ManageExpense Screen</Text>;
 }
 
