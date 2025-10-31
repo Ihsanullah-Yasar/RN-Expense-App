5b483c3dbbcac74366c4cb129919a12425b854ce
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-10-31 23:46:24 +0430

    custome button compenent added

---
+++ components/UI/Button.js
@@ -0,0 +1,17 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function Button({ children, onPress }) {
+  return (
+    <View>
+      <Pressable onPress={onPress}>
+        <View>
+          <Text>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default Button;
+
+const styles = StyleSheet.create({});
