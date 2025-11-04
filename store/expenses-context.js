6249a5e0b9f6cecbd7b4212563dc187ce27e764b
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-04 22:53:48 +0430

    provider value added

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -110,7 +110,18 @@ function ExpensesContextProvider({ children }) {
     dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
   }
 
-  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const value = {
+    expenses: expensesState,
+    addExpenses: addExpenses,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 
 export default ExpensesContextProvider;
