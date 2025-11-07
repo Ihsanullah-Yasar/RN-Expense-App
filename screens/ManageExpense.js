be87fc7df8ebd1cb1d03adb2f3c5e99396e3af37
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-07 18:53:06 +0430

    style removed

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -65,27 +65,3 @@ function ManageExpense({ route, navigation }) {
 }
 
 export default ManageExpense;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: GlobalStyles.colors.primary800,
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    minWidth: 120,
-    marginHorizontal: 8,
-  },
-  deleteContainer: {
-    padding: 8,
-    marginTop: 16,
-    borderTopWidth: 2,
-    borderTopColor: GlobalStyles.colors.primary200,
-    alignItems: "center",
-  },
-});
