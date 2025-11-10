5691da793b29b5ff78156e573b66c4e790d07fff
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-10 15:25:11 +0430

    style and design improved

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -19,3 +19,20 @@ function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
+    fontWeight: "bold",
+  },
+});
