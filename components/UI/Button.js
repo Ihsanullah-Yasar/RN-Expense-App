72cb766d58ed0e3c0b105de83110ad7c7e8e3f38
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-11 15:27:04 +0430

    style and design improved

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -19,3 +19,26 @@ function Button({ children, onPress, mode, style }) {
 }
 
 export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200,
+  },
+  pressed: {
+    opacity: 7.5,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+  },
+});
