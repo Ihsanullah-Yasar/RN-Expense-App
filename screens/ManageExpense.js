faffc3d1dd622e65f4d743460b04df8d76bb759c
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-14 15:11:21 +0430

    expense form consumed in the component

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/iconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/manageExpense/expenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expenseCtx = useContext(ExpensesContext);
@@ -42,6 +43,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
