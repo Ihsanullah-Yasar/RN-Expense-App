7fa10877eed3e107e7f8a7db1cd5e18a9536739b
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-18 23:39:24 +0430

    style: design improved

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -4,22 +4,28 @@ import Input from "./Input";
 function ExpenseForm() {
   function amountChangeHandler() {}
   return (
-    <View style={styles.inputRow}>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text>Your Expense</Text>
+      <View style={inputRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+
       <Input
         label="Description"
         textInputConfig={{
@@ -35,8 +41,21 @@ function ExpenseForm() {
 export default ExpenseForm;
 
 const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+    textAlign: "center",
+  },
   inputRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
   },
 });
