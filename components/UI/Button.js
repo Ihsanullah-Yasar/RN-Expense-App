dc5e7e51877d096abb8e62780cd2c3e709f14741
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-11-22 19:50:11 +0430

    style changed

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -36,9 +36,4 @@ const styles = StyleSheet.create({
   flatText: {
     color: GlobalStyles.colors.primary200,
   },
-  pressed: {
-    opacity: 7.5,
-    backgroundColor: GlobalStyles.colors.primary100,
-    borderRadius: 4,
-  },
 });
