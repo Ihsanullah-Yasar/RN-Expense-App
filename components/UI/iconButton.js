118303cc9af4dee205ef38b2ec11f5e02847eb15
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-12-06 15:17:27 +0430

    style: margin removed

--- components/UI/iconButton.js
+++ components/UI/iconButton.js
@@ -20,8 +20,6 @@ const styles = StyleSheet.create({
   buttonContainer: {
     borderRadius: 24,
     padding: 6,
-    marginHorizontal: 8,
-    marginVertical: 2,
   },
   pressed: {
     opacity: 0.75,
