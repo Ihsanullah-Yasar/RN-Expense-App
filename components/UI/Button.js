c89496367702d948241f29a70fdc15c7ec644b60
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-07 14:55:05 +0430

    style: btn bg transparency removed

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -26,9 +26,6 @@ const styles = StyleSheet.create({
     padding: 8,
     backgroundColor: GlobalStyles.colors.primary500,
   },
-  flat: {
-    backgroundColor: "transparent",
-  },
   buttonText: {
     color: "white",
     textAlign: "center",
