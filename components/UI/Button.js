58a36e95d29a8b8137bbd10d4986f4833dd96f41
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-08 14:57:39 +0430

    style: button bg transparency added

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -26,6 +26,9 @@ const styles = StyleSheet.create({
     padding: 8,
     backgroundColor: GlobalStyles.colors.primary500,
   },
+  flat: {
+    backgroundColor: "transparent",
+  },
   buttonText: {
     color: "white",
     textAlign: "center",
