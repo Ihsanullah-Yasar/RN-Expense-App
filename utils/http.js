57a01998d1efba95a9053def067601ece0afbe4c
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-22 17:52:55 +0430

    expenses fetching feature added and make it async

--- utils/http.js
+++ utils/http.js
@@ -5,3 +5,19 @@ const backendUrl = "https://rn-expense-app-95159-default-rtdb.firebaseio.com";
 export function storeExpense(expenseData) {
   axios.post(`${backendUrl}/expenses.json`, expenseData);
 }
+
+export async function fetchExpenses() {
+  const response = await axios.get(`${backendUrl}/expenses.json`);
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      data: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
+}
