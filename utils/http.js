e6a4218067d4d129f47dd8f162d8f68307b0d5aa
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-26 23:11:49 +0430

    fix: case and code typos fixed

--- utils/http.js
+++ utils/http.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const backendUrl = "https://rn-expense-app-95159-default-rtdb.firebaseio.com";
+const BACKEND_URL = "https://rn-expense-app-95159-default-rtdb.firebaseio.com";
 
 export function storeExpense(expenseData) {
-  axios.post(`${backendUrl}/expenses.json`, expenseData);
+  axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
 }
 
 export async function fetchExpenses() {
-  const response = await axios.get(`${backendUrl}/expenses.json`);
+  const response = await axios.get(`${BACKEND_URL}/expenses.json`);
   const expenses = [];
 
   for (const key in response.data) {
     const expenseObj = {
       id: key,
       amount: response.data[key].amount,
-      data: new Date(response.data[key].date),
+      date: new Date(response.data[key].date),
       description: response.data[key].description,
     };
     expenses.push(expenseObj);
