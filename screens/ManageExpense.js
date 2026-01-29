2e6c3ed7008150a38924ef4867d0b5ded66a6b53
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-29 23:43:37 +0430

    updated the submit handler

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,7 +4,7 @@ import IconButton from "../components/UI/iconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
-import { storeExpense } from "../utils/http";
+import { deleteExpense, storeExpense, updateExpense } from "../utils/http";
 
 function ManageExpense({ route, navigation }) {
   const expenseCtx = useContext(ExpensesContext);
@@ -21,7 +21,8 @@ function ManageExpense({ route, navigation }) {
     });
   }, [isEditing, navigation]);
 
-  function deleteHandler() {
+  async function deleteHandler() {
+    await deleteExpense(editingExpenseId);
     expenseCtx.deleteExpense(editingExpenseId);
     navigation.goBack();
   }
@@ -33,6 +34,7 @@ function ManageExpense({ route, navigation }) {
   async function confirmHandler(expenseData) {
     if (isEditing) {
       expenseCtx.updateExpense(editingExpenseId, expenseData);
+      await updateExpense(editingExpenseId, expenseData);
     } else {
       const id = await storeExpense(expenseData);
       expenseCtx.addExpense({ ...expenseData, id });
