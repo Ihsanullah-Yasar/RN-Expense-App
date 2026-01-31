daaae839c028b4f6799e16c9f55692c3821473ac
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-01-31 19:55:00 +0430

    activity indicator added for loading

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -4,24 +4,34 @@ import { useContext, useEffect, useState } from "react";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../utils/date";
 import { fetchExpenses } from "../utils/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesCtx = useContext(ExpensesContext);
   // const [fetchedExpenses, setFetchedExpenses] = useState([]);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
       // setFetchedExpenses(expenses);
       expensesCtx.setExpenses(expenses);
+      setIsFetching(false);
     }
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
     return expense.date >= date7DaysAgo && expense.date <= today;
   });
+
   return (
     <ExpensesOutput
       expenses={recentExpenses}
